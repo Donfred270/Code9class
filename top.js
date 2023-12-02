d88efbb8942cbdffdddb4e16d7065816565bb4c7
Donfred VALLON <donfredvallon@gmail.com>
2023-12-02 14:04:33 -0500

    premye exo nan battle lan

--- top.js
+++ top.js
@@ -1,4 +1,6 @@
-let chaine = "Upwork profile upgrade"
-function inverseCaracter(a){
-    inverse = 
+let string = "battle la se pou jodi a";
+function retireEspace(a){
+    teksSansEspace = a.split(' ');
+    return `Men teks la san espas : " ${teksSansEspace} "`;
 }
+console.log(retireEspace(string));
